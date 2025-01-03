a6dd8a093bd1f23ad53a0601d4973f99669847aa
Yuito-Bandai <yuiton1107@icloud.com>
2025-01-03 18:20:31 +0900

    fetch部分のURLにparamsを追加で渡した、検索フォームの追加

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BookList from '../components/Book/BookList.tsx';
 import '../styles/HomePage.css';
 
 const HomePage: React.FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <body>
-      <div className="homepage-container">
-        <h1 className="homepage-title">Tech Book Review App</h1>
+    <>
+      <header className="navbar">
+        <div className="navbar-container">
+          <h1 className="homepage-title">Tech Book Review App</h1>
+          <button className="hamburger-menu" onClick={toggleMenu}>
+            ☰
+          </button>
+          <nav className={`menu ${isMenuOpen ? 'open' : ''}`}>
+            <ul>
+              <li><a href="#home">Home</a></li>
+              <li><a href="#reviews">Reviews</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#contact">Contact</a></li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+      <main className="homepage-container">
         <BookList />
-      </div>
-    </body>
+      </main>
+    </>
   );
 };
 
