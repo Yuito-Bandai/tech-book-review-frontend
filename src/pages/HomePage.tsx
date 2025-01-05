f50b87429f881995e485dcb7209c5414809d6ef9
Yuito-Bandai <yuiton1107@icloud.com>
2025-01-05 17:33:46 +0900

    デザインの変更

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -25,8 +25,7 @@ const HomePage: React.FC<HomePageProps> = ({ onLogout }) => {
             <ul>
               <li><a href="#home">Home</a></li>
               <li><a href="#reviews">Reviews</a></li>
-              <li><a href="#about">About</a></li>
-              <li><a href="#contact">Contact</a></li>
+              <li><a href="#bookmark">bookmark</a></li>
               <li><button onClick={onLogout}>Logout</button></li>  {/* ログアウトボタン */}
             </ul>
           </nav>
