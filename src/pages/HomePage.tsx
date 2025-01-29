e887f90fc3268d840e8c16de508134e5d97bf682
Yuito-Bandai <yuiton1107@icloud.com>
2025-01-29 23:38:23 +0900

    ハンバーガーメニューを調整

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import BookList from '../components/Book/BookList.tsx';
 import '../styles/HomePage.css';
 
+// FontAwesomeIconをインポート
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
 interface HomePageProps {
   onLogout: () => void;  // ログアウト関数を受け取る
 }
@@ -19,13 +23,14 @@ const HomePage: React.FC<HomePageProps> = ({ onLogout }) => {
         <div className="navbar-container">
           <h1 className="homepage-title">Tech Book Review App</h1>
           <button className="hamburger-menu" onClick={toggleMenu}>
-            ☰
+            {/* FontAwesomeIconでハンバーガーメニューアイコンを表示 */}
+            <FontAwesomeIcon icon={faBars} />
           </button>
           <nav className={`menu ${isMenuOpen ? 'open' : ''}`}>
             <ul>
               <li><a href="#home">Home</a></li>
               <li><a href="#reviews">Reviews</a></li>
-              <li><a href="#bookmark">bookmark</a></li>
+              <li><a href="#bookmark">Bookmark</a></li>
               <li><button onClick={onLogout}>Logout</button></li>  {/* ログアウトボタン */}
             </ul>
           </nav>
